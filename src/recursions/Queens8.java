660f147cbaf9aa8df82b9b8cad2a8f2a994d2b78
Dimitar Rachev <DesolateBG@gmail.com>
2022-05-14 20:10:31 +0300

    8 queens puzzle

---
+++ src/recursions/Queens8.java
@@ -0,0 +1,78 @@
+package recursions;
+
+public class Queens8 {
+    public static char[][] board = populateBoard();
+
+    private static char[][] populateBoard() {
+        char[][] initial = new char[8][8];
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                initial[i][j] = '-';
+            }
+        }
+        return initial;
+    }
+
+    public static void main(String[] args) {
+
+        placeQueen(0);
+
+    }
+
+    private static void placeQueen(int row) {
+        if (row == 8) {
+            printBoard();
+            return;
+        }
+        for (int col = 0; col < 8; col++) {
+            if (placeIsAvailable(row, col)) {
+                putQueen(row, col);
+                placeQueen(row + 1);
+                removeQueen(row, col);
+            }
+        }
+
+    }
+
+    private static void removeQueen(int row, int col) {
+        board[row][col] = '-';
+    }
+
+    private static void printBoard() {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    private static void putQueen(int row, int col) {
+        board[row][col] = '*';
+    }
+
+    private static boolean placeIsAvailable(int row, int col) {
+        int r = row, c = col;
+        while (r >= 0) {
+            if (board[r--][col] == '*') {
+                return false;
+            }
+        }
+
+        r = row;
+        while (r >= 0 && c >= 0) {
+            if (board[r--][c--] == '*') {
+                return false;
+            }
+        }
+        r = row;
+        c = col;
+        while (r >= 0 && c < 8) {
+            if (board[r--][c++] == '*') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
