68678d260faa2b39e80dcac4b22467f876c46c45
Dimitar Rachev <DesolateBG@gmail.com>
2022-05-26 20:10:40 +0300

    stacks and queues

--- src/Misc/Arr.java
+++ src/Misc/Arr.java
@@ -8,11 +8,9 @@ public class Arr {
         int[] arr1 = new int[]{3, 5, 7, 9, 1, 67};
         int[] arr2 = new int[]{6, 65, 7, 1, 5, 67, 65, 65, 574};
 
-//        1 3 5 7 9 67
-//        1 6 7 65 65 65 67 574
-
         Arrays.sort(arr1);
         Arrays.sort(arr2);
+
         for (int i = 0; i < arr1.length; i++) {
             int el = arr1[i];
             if (isPresent(el, arr2, 0, arr2.length - 1)) {
@@ -25,6 +23,7 @@ public class Arr {
         if (start == end) {
             return el == arr2[start];
         }
+
         int middle = (end + start) / 2;
         if (el > arr2[middle]) {
             return isPresent(el, arr2, middle + 1, end);
@@ -34,5 +33,4 @@ public class Arr {
             return true;
         }
     }
-
 }
